04906371b129c513b7616d5e1c34d3262b907213
Metko <metko61@abv.bg>
2020-02-23 12:17:32 +0200

    visual studio

---
+++ Pentakill/Pentakill/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace Pentakill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product(1, "Test product", 100.01m, 100, new DateTime(2019, 06, 30));
+            string json = JsonConvert.SerializeObject(product);
+            Console.WriteLine("Single product object");
+            Console.WriteLine(json);
+            Console.WriteLine(new string('-',50));
+        }
+    }
+}
